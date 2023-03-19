ff801e786e36dba1f46e9df586b0180cb41a660f
HJeong1200 <hjeong1200@gmail.com>
2023-03-19 23:58:21 +0900

    feat: layout for Showcase header

--- src/components/showcase/ShowcaseHeader.tsx
+++ src/components/showcase/ShowcaseHeader.tsx
@@ -1,10 +1,26 @@
+import { Avatar } from "@mui/material";
+import styled from "styled-components";
+
 const ShowcaseHeader = () => {
   return (
-    <>
-      <div>Img</div>
-      <div>UserName</div>
-    </>
+    <Wrapper>
+      <Avatar>H</Avatar>
+      <NameContainer>UserName</NameContainer>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  padding: 20px;
+  background-color: aliceblue;
+`;
+
+const NameContainer = styled.div`
+  margin-left: 10px;
+`;
 
 export default ShowcaseHeader;
