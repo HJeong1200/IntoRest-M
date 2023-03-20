9423bc18056bd58e217393b221284e60ac695945
HJeong1200 <hjeong1200@gmail.com>
2023-03-20 15:40:21 +0900

    feat: create showcase create page

---
+++ src/pages/showcase/ShowcaseCreate.tsx
@@ -0,0 +1,49 @@
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase/firebase";
+import { Button } from "@mui/material";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import { useState } from "react";
+import styled from "styled-components";
+
+const ShowcaseCreate = () => {
+  const [content, setContent] = useState<string>("");
+
+  const handleUpload = () => {
+    if (!content) return;
+  };
+
+  return (
+    <Wrapper>
+      <h2>New Showcase</h2>
+      <Box component="form" sx={sx}>
+        <TextField disabled variant="standard" label="Image" />
+        <TextField
+          variant="standard"
+          multiline
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+          label="Content"
+        />
+      </Box>
+      <Button onClick={handleUpload}>Upload</Button>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const sx = {
+  display: "flex",
+  flexDirection: "column",
+  width: "80%",
+  marginBottom: "25px",
+};
+
+export default ShowcaseCreate;
